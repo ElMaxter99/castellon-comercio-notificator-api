0176ff7b7ff3f7805c713674c3f1e5121e3a50ac
Alvaro Maxter <themaxter99@gmail.com>
2025-11-03 15:40:02 +0100

    change request color fomr blue to white

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -15,7 +15,7 @@ const COLORS = {
   WARN: "\x1b[33m", // yellow
   ERROR: "\x1b[31m", // red
   DEBUG: "\x1b[35m", // magenta
-  REQUEST: "\x1b[34m", // blue
+  REQUEST: "\x1b[37m", // white
   RESET: "\x1b[0m",
 };
 
